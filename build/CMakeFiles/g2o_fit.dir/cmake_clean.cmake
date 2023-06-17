f1ab748f7c01384f5a2d1a9399d4d1afdc5f0bbd
linzs-online <linzs.online@gmail.com>
2023-06-17 09:51:50 +0800

    first commit

---
+++ build/CMakeFiles/g2o_fit.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/g2o_fit.dir/g2o_fit.cpp.o"
+  "CMakeFiles/g2o_fit.dir/g2o_fit.cpp.o.d"
+  "g2o_fit"
+  "g2o_fit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/g2o_fit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/g2o_fit.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for g2o_fit.
+# This may be replaced when dependencies are built.
